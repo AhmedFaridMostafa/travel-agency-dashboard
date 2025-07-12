fde2bd9c80d3f1be01f973c7a890e5f482d01e98
Ahmed Fared <ahmed.super997@gmail.com>
2025-07-12 03:57:55 +0300

    Initial commit from create-react-router

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { registerLicense } from "@syncfusion/ej2-base";
+import "./assets/styles/index.css";
+import App from "./App.tsx";
+
+registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE_KEY);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
